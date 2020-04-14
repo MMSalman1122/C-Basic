b7313a5c79c4e01fda4a88f0074da54deaf6baaf
MMSalman1122 <43203395+MMSalman1122@users.noreply.github.com>
2020-04-14 13:18:50 +0500

    Section 4 Lecture 3

---
+++ enums.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    public enum ShippingMethod
+    {
+        Regular=1,
+        Registered=2,
+        Express=3
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = ShippingMethod.Express;
+
+            Console.WriteLine(method);
+
+            // method to number
+            Console.WriteLine((int)method);
+
+            // number to method
+            var num = 3;
+            Console.WriteLine((ShippingMethod)num);
+
+            //metho to string
+            Console.WriteLine(method.ToString());
+
+            //parsing
+            var method_name = "Express";
+            var ShippingMethod1=(ShippingMethod)Enum.Parse(typeof(ShippingMethod), method_name);
+
+            while (true)
+            {
+
+            }
+        }
+    }
+}
