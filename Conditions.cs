9b09921d8faf03efafe2860a60e717609d73f7ea
MMSalman1122 <43203395+MMSalman1122@users.noreply.github.com>
2020-04-15 13:02:59 +0500

    Section 4 lec 42

---
+++ Conditions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace test
+{
+    public enum Seasons
+    {
+        Spring,
+        Summer,
+        Automun,
+        Winter
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var season1 = Seasons.Automun;
+            
+            switch(season1)
+            {
+                case Seasons.Automun:
+                    Console.WriteLine("Its beautiful season.");
+                    break;
+                case Seasons.Summer:
+                    Console.WriteLine("go to beach");
+                    break;
+                default:
+                    Console.WriteLine("No season");
+                    break;
+                
+            }
+            
+
+            while (true)
+            {
+
+            }
+        }
+    }
+}
